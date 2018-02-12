0a25c737880947ca2db959d491fee76498418cfd
Artem Labazin <Artem.Labazin@infobip.com>
2018-02-12 20:02:28 +0300

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Echo.Mixfile do
     [
       app: :echo,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.4.5",
       start_permanent: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       escript: [main_module: Echo.CLI],
